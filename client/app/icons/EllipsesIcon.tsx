562058086d09d81a820b4067fe65a729c31d497c
Leo Villalobos <villaleobos@gmail.com>
2025-04-24 00:09:57 -0700

    feat: add icons client components

---
+++ client/app/icons/EllipsesIcon.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { MouseEventHandler } from "react";
+
+interface EllipsesIconProps {
+  className?: string;
+  onClick?: MouseEventHandler<HTMLSpanElement>;
+}
+
+export default function EllipsesIcon({
+  className,
+  onClick,
+}: EllipsesIconProps) {
+  return (
+    <span className={className} onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+        />
+      </svg>
+    </span>
+  );
+}
